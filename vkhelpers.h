81fda10b6975d21612f95216f6cd2f9d874e8283
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2017-12-18 13:05:29 +0100

    set_image_layout_subres

--- vkhelpers.h
+++ vkhelpers.h
@@ -14,12 +14,12 @@
 
 #define VK_CHECK_RESULT(f) 																				\
 {																										\
-	VkResult res = (f);																					\
-	if (res != VK_SUCCESS)																				\
-	{																									\
-		printf("Fatal : VkResult is %d in %s at line %d\n", res,  __FILE__, __LINE__); \
-		assert(res == VK_SUCCESS);																		\
-	}																									\
+    VkResult res = (f);																					\
+    if (res != VK_SUCCESS)																				\
+    {																									\
+        printf("Fatal : VkResult is %d in %s at line %d\n", res,  __FILE__, __LINE__); \
+        assert(res == VK_SUCCESS);																		\
+    }																									\
 }
 
 typedef struct _vkh_device_t*   VkhDevice;
@@ -28,18 +28,18 @@ typedef struct _vkh_buffer_t*   VkhBuffer;
 
 ///////////////////////////////
 VkhImage vkh_image_create       (VkhDevice pDev, VkFormat format, uint32_t width, uint32_t height, VkImageTiling tiling,
-									VkMemoryPropertyFlags memprops,	VkImageUsageFlags usage, VkImageLayout layout);
+                                    VkMemoryPropertyFlags memprops,	VkImageUsageFlags usage, VkImageLayout layout);
 VkhImage vkh_image_ms_create    (VkhDevice pDev, VkFormat format, VkSampleCountFlagBits num_samples, uint32_t width, uint32_t height,
-									VkMemoryPropertyFlags memprops,	VkImageUsageFlags usage, VkImageLayout layout);
+                                    VkMemoryPropertyFlags memprops,	VkImageUsageFlags usage, VkImageLayout layout);
 VkhImage vkh_tex2d_array_create (VkhDevice pDev, VkFormat format, uint32_t width, uint32_t height, uint32_t layers,
-									VkMemoryPropertyFlags memprops, VkImageUsageFlags usage, VkImageLayout layout);
+                                    VkMemoryPropertyFlags memprops, VkImageUsageFlags usage, VkImageLayout layout);
 void vkh_image_create_descriptor(VkhImage img, VkImageViewType viewType, VkImageAspectFlags aspectFlags, VkFilter magFilter, VkFilter minFilter,
-									VkSamplerMipmapMode mipmapMode);
+                                    VkSamplerMipmapMode mipmapMode);
 void vkh_image_destroy          (VkhImage img);
 
 ////////////////////////////////
 VkhBuffer   vkh_buffer_create   (VkhDevice pDev, VkBufferUsageFlags usage,
-									VkMemoryPropertyFlags memoryPropertyFlags, VkDeviceSize size);
+                                    VkMemoryPropertyFlags memoryPropertyFlags, VkDeviceSize size);
 void        vkh_buffer_destroy  (VkhBuffer buff);
 VkResult    vkh_buffer_map      (VkhBuffer buff);
 void        vkh_buffer_unmap    (VkhBuffer buff);
@@ -56,17 +56,20 @@ void vkh_cmd_begin  (VkCommandBuffer cmdBuff, VkCommandBufferUsageFlags flags);
 void vkh_cmd_end    (VkCommandBuffer cmdBuff);
 void vkh_cmd_submit (VkQueue queue, VkCommandBuffer *pCmdBuff, VkFence fence);
 void vkh_cmd_submit_with_semaphores(VkQueue queue, VkCommandBuffer *pCmdBuff, VkSemaphore waitSemaphore,
-									VkSemaphore signalSemaphore, VkFence fence);
+                                    VkSemaphore signalSemaphore, VkFence fence);
 
 VkPhysicalDevice vkh_find_phy (VkInstance inst, VkPhysicalDeviceType phyType);
 
 VkShaderModule vkh_load_module(VkDevice dev, const char* path);
 
 void        set_image_layout(VkCommandBuffer cmdBuff, VkImage image, VkImageAspectFlags aspectMask, VkImageLayout old_image_layout,
-					  VkImageLayout new_image_layout, VkPipelineStageFlags src_stages, VkPipelineStageFlags dest_stages);
+                      VkImageLayout new_image_layout, VkPipelineStageFlags src_stages, VkPipelineStageFlags dest_stages);
+void        set_image_layout_subres(VkCommandBuffer cmdBuff, VkImage image, VkImageSubresourceRange subresourceRange, VkImageLayout old_image_layout,
+                      VkImageLayout new_image_layout, VkPipelineStageFlags src_stages, VkPipelineStageFlags dest_stages);
+
 
 bool        memory_type_from_properties(VkPhysicalDeviceMemoryProperties* memory_properties, uint32_t typeBits,
-										VkFlags requirements_mask, uint32_t *typeIndex);
+                                        VkFlags requirements_mask, uint32_t *typeIndex);
 char *      read_spv(const char *filename, size_t *psize);
 uint32_t*   readFile(uint32_t* length, const char* filename);
 
