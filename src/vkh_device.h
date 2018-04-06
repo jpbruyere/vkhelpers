e5333027ccbb7bb1c1e11e1164329530eac1da96
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2018-04-06 04:44:01 +0200

    force correct fPic for both static and shared

--- src/vkh_device.h
+++ src/vkh_device.h
@@ -4,10 +4,13 @@
 #include "vkh.h"
 
 typedef struct _vkh_device_t{
-    VkPhysicalDevice    phy;
-    VkDevice            dev;
-
+    VkDevice				dev;
     VkPhysicalDeviceMemoryProperties phyMemProps;
+    VkRenderPass			renderPass;
+//    VkPhysicalDevice    phy;
+//    VkDevice            dev;
+
+//    VkPhysicalDeviceMemoryProperties phyMemProps;
 }vkh_device_t;
 
 #endif
