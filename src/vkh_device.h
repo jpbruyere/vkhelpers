87e1a713755fd58e2bb2e47af9568583ed99d8dd
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2020-08-13 12:46:57 +0200

    remove old VK_EXT_debug_report function pointers

--- src/vkh_device.h
+++ src/vkh_device.h
@@ -29,16 +29,12 @@ extern "C" {
 #include "vkh.h"
 #include "vk_mem_alloc.h"
 
-PFN_vkCreateDebugReportCallbackEXT  CreateDebugReportCallback;
-PFN_vkDestroyDebugReportCallbackEXT DestroyDebugReportCallback;
-PFN_vkDebugReportMessageEXT         dbgBreakCallback;
-
 typedef struct _vkh_device_t{
-    VkDevice				dev;
-    VkPhysicalDeviceMemoryProperties phyMemProps;
-    VkPhysicalDevice        phy;
-    VmaAllocator            allocator;
-    VkInstance              instance;
+	VkDevice				dev;
+	VkPhysicalDeviceMemoryProperties phyMemProps;
+	VkPhysicalDevice        phy;
+	VmaAllocator            allocator;
+	VkInstance              instance;
 }vkh_device_t;
 
 #ifdef __cplusplus
