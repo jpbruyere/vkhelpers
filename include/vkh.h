82989c8bd67ac81b90a25481172cdb5f55135459
LinuxLegend <Tyler@linuxdev.app>
2022-03-28 04:56:55 -0600

    Switched to STDC_NO_THREADS

--- include/vkh.h
+++ include/vkh.h
@@ -30,7 +30,7 @@ extern "C" {
 
 #include <vulkan/vulkan.h>
 
-#if defined(_WIN32) || defined(WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(WIN64) || defined(__WIN64__)
+#if defined(STDC_NO_THREADS)
 #include "deps/tinycthread.h"
 #else
 // For platforms outside of Windows, Threads.h is provided for by compiler that supports C11 standard.
