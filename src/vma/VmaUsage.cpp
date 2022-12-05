bede6aeb4264040784a422e64796ce21a012eab5
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2022-12-05 03:04:41 +0100

    vma as static library, linkage in c

--- src/vma/VmaUsage.cpp
+++ src/vma/VmaUsage.cpp
@@ -5,5 +5,5 @@ vk_mem_alloc.h to include definitions of its internal implementation
 #pragma warning(push)
 #pragma warning(disable:4100;disable:4127;disable:4324)
 #define VMA_IMPLEMENTATION
-#include "vk_mem_alloc.h"
+#include "../vk_mem_alloc.h"
 #pragma warning(pop)
