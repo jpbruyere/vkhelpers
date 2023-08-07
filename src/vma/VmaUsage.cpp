18059c4c6464ff13ffb6764c8a882cf66d0676f5
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2023-08-07 16:49:40 +0200

    add include cstdio for vma

--- src/vma/VmaUsage.cpp
+++ src/vma/VmaUsage.cpp
@@ -5,5 +5,6 @@ vk_mem_alloc.h to include definitions of its internal implementation
 #pragma warning(push)
 #pragma warning(disable:4100;disable:4127;disable:4324)
 #define VMA_IMPLEMENTATION
+#include <cstdio>
 #include "../vk_mem_alloc.h"
 #pragma warning(pop)
